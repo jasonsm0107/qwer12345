0d9d64e611fc3f4a2afb2a2d1930856351c32837
Elysia <jasonsm0107@gmail.com>
2025-01-23 03:10:42 +0900

    first commit

---
+++ src/core/core.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <jni.h>
+#include <memory>
+#include <string>
+#include "../config/config_manager.h"
+#include "../resource/resource_manager.h"
+#include "../utils/logger.h"
+
+class PixelmonUtilsCore {
+private:
+    static std::unique_ptr<PixelmonUtilsCore> instance;
+    JavaVM* jvm;
+    bool initialized;
+
+    std::unique_ptr<ConfigManager> configManager;
+    std::unique_ptr<ResourceManager> resourceManager;
+    std::unique_ptr<Logger> logger;
+
+    PixelmonUtilsCore() : jvm(nullptr), initialized(false) {}
+
+public:
+    static PixelmonUtilsCore& getInstance() {
+        if (!instance) {
+            instance = std::unique_ptr<PixelmonUtilsCore>(new PixelmonUtilsCore());
+        }
+        return *instance;
+    }
+
+    bool initialize(JNIEnv* env);
+    void cleanup();
+    bool isInitialized() const { return initialized; }
+
+    ConfigManager* getConfigManager() { return configManager.get(); }
+    ResourceManager* getResourceManager() { return resourceManager.get(); }
+    Logger* getLogger() { return logger.get(); }
+};
